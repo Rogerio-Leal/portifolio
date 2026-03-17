50c731af510ddec1591a0e83c04c50fa602e2025
rogério <rogerionarcizoleal@gmail.com>
2026-03-16 21:00:38 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../componets/Avatar'
+import Paragrafo from '../../componets/Paragrafo'
 import Titulo from '../../componets/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Rogerio-Leal.png" />
-    <Titulo fontSize={20}>Rogério Leal</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Rogério Leal</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Rogerio-Leal
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full Stack Java
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
